7fce1f8d4be85e6f106d944ebb5437ce202bed2e
Jovanie Cabatuan <jhovaniecabatuan@gmail.com>
2026-06-16 18:23:20 +0800

    initialize project

---
+++ src/main/java/jcss/soft/com/superproxy/SuperproxyApplication.java
@@ -0,0 +1,13 @@
+package jcss.soft.com.superproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperproxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jcss/soft/com/superproxy/SuperproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package jcss.soft.com.superproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
